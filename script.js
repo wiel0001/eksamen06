6cda5dc9c51338b9bf5d4c6633ac693c5f8bb9e8
wiel0001 <wiel0001@stud.kea.dk>
2025-06-11 09:30:42 +0200

    Final push

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
